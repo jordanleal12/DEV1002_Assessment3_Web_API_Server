758caeb2e1f8c6687fe52517f8e82ab1d4093ddb
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-08-17 14:40:57 +1000

    feat(addresses_controllers): created basic GET request routes to handle single and multiple address instances, passing `test_get_address` and `test_get_addresses`

--- src/controllers/addresses_controllers.py
+++ src/controllers/addresses_controllers.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, request, jsonify, abort
-from sqlalchemy.exc import IntegrityError
-from marshmallow.exceptions import ValidationError
-from psycopg2 import errorcodes
-from extensions import db
-from models import Address
-from schemas import address_schema
+"""Controller for defining routes with CRUD functionality for addresses table"""
 
+from flask import Blueprint, request, jsonify, abort
+from sqlalchemy.exc import IntegrityError  # DB level errors
+from marshmallow.exceptions import ValidationError  # Schema level errors
+from psycopg2 import errorcodes  # For specific DB level error messages
+from extensions import db  # SQLAlchemy instance
+from models import Address  # Address model
+from schemas import address_schema, addresses_schema  # Address schemas
+
+# Allows importing routes to main via blueprints
 addresses = Blueprint("addresses", __name__, url_prefix="/addresses")
 
 
@@ -47,3 +50,31 @@ def create_address():
             "error": "Database Integrity Error",
             "message": str(e.orig),
         }, 400  # General error message for miscellaneous integrity issues
+
+
+@addresses.route("", methods=["GET"])
+def get_addresses():
+    """Fetch all address instances from db using GET request."""
+
+    stmt = db.select(Address)  # Create SQL SELECT object for querying address table
+    # Get addresses as list of scalar objects
+    addresses_list = db.session.scalars(stmt).all()
+    data = addresses_schema.dump(addresses_list)  # Deserialize to dict with schema
+
+    if data:
+        return jsonify(data)  # Return Json of all address instances
+    else:
+        return {"message": "No address records found."}, 404  # Error msg for no records
+
+
+@addresses.route("/<int:address_id>", methods=["GET"])
+def get_address(address_id):
+    """Fetch single address instance from db using GET request."""
+
+    address = db.session.get(Address, address_id)
+    data = address_schema.dump(address)
+
+    if data:
+        return jsonify(data)
+    else:
+        return {"message": "No address record found."}, 404
